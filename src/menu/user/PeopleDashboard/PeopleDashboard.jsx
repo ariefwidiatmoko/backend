2c5ade9b87970c092c54a82f38a66e2dda884876
Arief Widiatmoko <ariefwidiatmoko@gmail.com>
2019-07-23 21:51:17 +0700

    End of Section 26

--- src/menu/user/PeopleDashboard/PeopleDashboard.jsx
+++ src/menu/user/PeopleDashboard/PeopleDashboard.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from 'react';
+import {connect} from 'react-redux';
+import {compose} from 'redux';
+import {firestoreConnect} from 'react-redux-firebase';
+import { Grid, Segment, Header, Card } from 'semantic-ui-react';
+import PersonCard from './PersonCard';
 
-const PeopleDashboard = () => {
-    return (
-        <div>
-            <h1>People Dashboard</h1>
-        </div>
-    )
-}
+const query = ({auth}) => {
+  return [
+    {
+      collection: 'users',
+      doc: auth.uid,
+      subcollections: [{collection: 'following'}],
+      storeAs: 'following'
+    },
+    {
+      collection: 'users',
+      doc: auth.uid,
+      subcollections: [{collection: 'followers'}],
+      storeAs: 'followers'
+    }
+  ];
+};
 
-export default PeopleDashboard
+const PeopleDashboard = ({followings, followers}) => {
+  return (
+    <Grid>
+      <Grid.Column width={16}>
+        {followers && followers[0] &&
+        <Segment>
+          <Header dividing content="People following me" />
+          <Card.Group itemsPerRow={8} stackable>
+            {followers.length > 0 && 
+              followers.map(follower => <PersonCard key={follower.id} user={follower} />)}
+          </Card.Group>
+        </Segment>}
+        {followings && followings[0] &&
+        <Segment>
+          <Header dividing content="People I'm following" />
+          <Card.Group itemsPerRow={8} stackable>
+            {followings.length > 0 && followings.map(following => <PersonCard key={following.id} user={following} />)}
+          </Card.Group>
+        </Segment>}
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+const mapStateToProps = state => ({
+  followings: state.firestore.ordered.following,
+  followers: state.firestore.ordered.followers,
+  auth: state.firebase.auth
+})
+
+export default compose(connect(mapStateToProps, null), firestoreConnect(props => query(props)))(PeopleDashboard);
--- src/menu/user/UserDetailed/UserDetailedPage.jsx
+++ src/menu/user/UserDetailed/UserDetailedPage.jsx
@@ -2,44 +2,61 @@ import React, { Component } from "react";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import { firestoreConnect, isEmpty } from "react-redux-firebase";
-import { Link } from "react-router-dom";
-import { Button, Grid, Segment } from "semantic-ui-react";
+import {Grid} from 'semantic-ui-react'
 import UserHeader from "./UserHeader";
 import UserAbout from "./UserAbout";
 import UserPhoto from "./UserPhoto";
 import UserEvents from "./UserEvents";
-import {userDetailedQuery as query} from "../../user/UserQueries"
-import LoadingComponent from '../../../app/layout/LoadingComponent'
-import {getUserEvents} from '../userActions'
+import UserSidebar from "./UserSidebar";
+import { userDetailedQuery as query } from "../../user/UserQueries";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { getUserEvents, followUser, unfollowUser } from "../userActions";
 
 class UserDetailedPage extends Component {
-
   async componentDidMount() {
     await this.props.getUserEvents(this.props.userUid, 3);
-  };
+  }
 
   changeTab = (e, data) => {
     this.props.getUserEvents(this.props.userUid, data.activeIndex);
-  }
-  
+  };
+
   render() {
-    const { auth, profile, photos, match, requesting, events, eventsLoading } = this.props;
+    const {
+      auth,
+      profile,
+      photos,
+      match,
+      requesting,
+      events,
+      eventsLoading, 
+      followUser, 
+      following, 
+      unfollowUser
+    } = this.props;
     const isCurrentUser = auth.uid === match.params.id;
     const loading = Object.values(requesting).some(a => a === true);
-    if (loading) return <LoadingComponent />
+    const isFollowing = !isEmpty(following);
+
+    if (loading) return <LoadingComponent />;
+
     return (
       <Grid>
         <UserHeader auth={auth} profile={profile} />
         <UserAbout profile={profile} />
-        <Grid.Column width={4}>
-          <Segment>
-            {isCurrentUser ?
-            <Button as={Link} to='/settings' color="teal" fluid basic content="Edit Profile" /> :
-            <Button color="teal" fluid basic content="Follow User" />}
-          </Segment>
-        </Grid.Column>
+        <UserSidebar 
+          isCurrentUser={isCurrentUser}
+          isFollowing={isFollowing} 
+          profile={profile} 
+          followUser={followUser} 
+          unfollowUser={unfollowUser}
+          />
         {photos && photos.length > 0 && <UserPhoto photos={photos} />}
-        <UserEvents events={events} eventsLoading={eventsLoading} changeTab={this.changeTab} />
+        <UserEvents
+          events={events}
+          eventsLoading={eventsLoading}
+          changeTab={this.changeTab}
+        />
       </Grid>
     );
   }
@@ -50,9 +67,11 @@ const mapStateToProps = (state, ownProps) => {
   let profile = {};
 
   if (ownProps.match.params.id === state.auth.uid) {
-    profile = state.firebase.profile
+    profile = state.firebase.profile;
   } else {
-    profile = !isEmpty(state.firestore.ordered.profile) && state.firestore.ordered.profile[0];
+    profile =
+      !isEmpty(state.firestore.ordered.profile) &&
+      state.firestore.ordered.profile[0];
     userUid = ownProps.match.params.id;
   }
   return {
@@ -62,18 +81,21 @@ const mapStateToProps = (state, ownProps) => {
     eventsLoading: state.async.loading,
     auth: state.firebase.auth,
     photos: state.firestore.ordered.photos,
-    requesting: state.firestore.status.requesting
+    requesting: state.firestore.status.requesting, 
+    following: state.firestore.ordered.following
   };
 };
 
 const mapDispatchToProps = {
-  getUserEvents
-}
+  getUserEvents, 
+  followUser, 
+  unfollowUser
+};
 
 export default compose(
   connect(
     mapStateToProps,
     mapDispatchToProps
   ),
-  firestoreConnect((auth, userUid) => query(auth, userUid))
+  firestoreConnect((auth, userUid, match) => query(auth, userUid, match))
 )(UserDetailedPage);
